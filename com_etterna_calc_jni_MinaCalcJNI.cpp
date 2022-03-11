b14f2989c77effc9c5ab86703b090aaed1b99e4b
Barinade <poco0317@users.noreply.github.com>
2022-03-10 19:15:29 -0600

    init

---
+++ com_etterna_calc_jni_MinaCalcJNI.cpp
@@ -0,0 +1,12 @@
+
+
+#include "MinaCalc/MinaCalc.h"
+#include <jni.h>
+
+JNIEXPORT jstring JNICALL Java_com_etterna_calc_jni_MinaCalcJNI_test(JNIEnv* env, jobject obj) {
+
+    std::string c = std::to_string(GetCalcVersion());
+
+    jstring result = env->NewStringUTF(c.c_str());
+    return result;
+}
