3a5eb77b662d4abaea185419de4b527df3849d74
Barinade <poco0317@users.noreply.github.com>
2023-05-31 19:12:43 -0500

    323

--- MinaCalc/MinaCalc.h
+++ MinaCalc/MinaCalc.h
@@ -2,8 +2,6 @@
 #include "../Models/NoteData/NoteDataStructures.h"
 #include <vector>
 
-// EXISTS FOR AUTOMATED COMPARISONS AGAINST THE NEW CALC ONLY
-
 // For internal, must be preprocessor defined
 #if defined(MINADLL_COMPILE) && defined(_WIN32)
 #define MINACALC_API __declspec(dllexport)
@@ -13,14 +11,16 @@
 #ifndef MINACALC_API
 #define MINACALC_API
 #endif
-
-  typedef std::vector<float>
-	SDiffs;
-typedef std::vector<SDiffs> MinaSD;
-
+typedef std::vector<std::vector<float>> MinaSD;
 typedef std::vector<std::vector<float>> Finger;
 typedef std::vector<Finger> ProcessedFingers;
 typedef std::vector<float> JackSeq;
+
+// number of pattern mods yes i know this is dumb help
+// we want to group stamina adjustment with the others for
+// display purposes but we don't want it when initializing pattern arrays
+// because it doesn't get generated until after we're done
+const static int ModCount = NUM_CalcPatternMod;
 
 /*	The difficulties of each hand tend to be independent from one another. This
 is not absolute, as in the case of polyrhythm trilling. However the goal of the
@@ -34,67 +34,75 @@ class Hand
 {
   public:
 	/*	Spits out a rough estimate of difficulty based on the ms values within
-	the interval The std::vector passed to it is the std::vector of ms values within each
-	interval, and not the full std::vector of intervals. */
-	float CalcMSEstimate(std::vector<float>& v);
-
-	// Wraps the three prepatory functions below
-	void InitHand(Finger& f1, Finger& f2, float ts);
+	the interval The vector passed to it is the vector of ms values within each
+	interval, and not the full vector of intervals. */
+	static float CalcMSEstimate(std::vector<float> input);
 
 	/*	Averages nps and ms estimates for difficulty to get a rough initial
 	value. This is relatively robust as patterns that get overrated by nps
 	estimates are underrated by ms estimates, and vice versa. Pattern modifiers
 	are used to adjust for circumstances in which this is not true. The result
-	is output to v_itvdiff. */
-	void InitDiff(Finger& f1, Finger& f2);
+	is output to v_itvNPSdiff and v_itvMSdiff. */
+	void InitBaseDiff(Finger& f1, Finger& f2);
+
+	// I don't know why this was ever being done in the internal loop, only the
+	// stam adjusted difficulties were dependent on a player_skill input, these
+	// values are static. Just calculate them for each skillset after pattern
+	// mods are done. For reasons we want to calculate stam mod on a different
+	// vector than what we apply the stam mod to, so calculate those as well.
+	// Yes this makes sense.
+	void InitAdjDiff();
 
 	// Totals up the points available for each interval
-	void InitPoints(Finger& f1, Finger& f2);
-
-	// Self explanatory
-	void SetTimingScale(float ts) { timingscale = ts; }
+	void InitPoints(const Finger& f1, const Finger& f2);
 
 	/*	The stamina model works by asserting a minimum difficulty relative to
 	the supplied player skill level for which the player's stamina begins to
 	wane. Experience in both gameplay and algorithm testing has shown the
 	appropriate value to be around 0.8. The multiplier is scaled to the
 	proportionate difference in player skill. */
-	std::vector<float> StamAdjust(float x, std::vector<float> diff);
+	// just recycle the stam_adj_diff vector directly in this function
+	// sometimes
+	void StamAdjust(float x, int ss, bool debug = false);
 
 	/*	For a given player skill level x, invokes the function used by wife
 	scoring to assert the average of the distribution of point gain for each
 	interval and then tallies up the result to produce an average total number
 	of points achieved by this hand. */
-	float CalcInternal(float x, bool stam, bool nps, bool js, bool hs);
+	// return value is true if a player can no longer reach scoregoal even if
+	// they get 100% of the remaining points
+	void CalcInternal(float& gotpoints,
+					  float& x,
+					  int ss,
+					  bool stam,
+					  bool debug = false);
 
-	std::vector<float> ohjumpscale;
-	std::vector<float> rollscale;
-	std::vector<float> hsscale;
-	std::vector<float> jumpscale;
-	std::vector<float> anchorscale;
-	std::vector<int> v_itvpoints;	// Point allotment for each interval
-	std::vector<float> v_itvNPSdiff; // Calculated difficulty for each interval
-	std::vector<float> v_itvMSdiff;  // Calculated difficulty for each interval
-	std::vector<float> debug;		 // debug info placement
-	std::vector<float> finalMSDvals; // cancer cancer cancer cancer
-	std::vector<float> pointslost;
+	std::vector<float> doot[ModCount];
+	std::vector<int> v_itvpoints; // Point allotment for each interval
+	std::vector<float>
+	  soap[NUM_CalcDiffValue]; // Calculated difficulty for each interval
+
+	// not necessarily self extraplanetary
+	// apply stam model to these (but output is sent to stam_adj_diff, not
+	// modified here)
+	std::vector<float> base_adj_diff[NUM_Skillset];
+	// but use these as the input for model
+	std::vector<float> base_diff_for_stam_mod[NUM_Skillset];
+
+	// pattern adjusted difficulty, allocate only once, stam needs to be based
+	// on the above, and it needs to be recalculated every time the player_skill
+	// value changes, again based on the above, technically we could use the
+	// skill_stamina element of the arrays to store this and save an allocation
+	// but that might just be too confusing idk
+	std::vector<float> stam_adj_diff;
+	std::vector<std::vector<std::vector<float>>> debugValues;
+
   private:
 	const bool SmoothDifficulty =
 	  true; // Do we moving average the difficulty intervals?
-
-	float timingscale; // Timingscale for use in the point proportion function
-	float jumpstreamscaler = 0.975f;
-	float handstreamscaler = 0.92f;
-	float finalscaler = 2.564f * 1.05f * 1.1f * 1.10f * 1.10f *
-						1.025f; // multiplier to standardize baselines
-
-	// Stamina Model params
-	const float ceil = 1.08f;	// stamina multiplier max
-	const float mag = 355.f;	 // multiplier generation scaler
-	const float fscale = 2000.f; // how fast the floor rises (it's lava)
-	const float prop =
-	  0.75f; // proportion of player difficulty at which stamina tax begins
 };
+
+struct JumpHandChordData;
 
 class Calc
 {
@@ -103,74 +111,115 @@ class Calc
 	hand objects and then runs the chisel function under varying circumstances
 	to estimate difficulty for each different skillset. Currently only
 	overall/stamina are being produced. */
-	std::vector<float> CalcMain(const std::vector<NoteInfo>& NoteInfo, float timingscale, float score_goal);
+	std::vector<float> CalcMain(const std::vector<NoteInfo>& NoteInfo,
+								float music_rate,
+								float score_goal);
 
-	// redo these asap
-	std::vector<float> JackStamAdjust(std::vector<float>& j, float x, bool jackstam);
-	float JackLoss(std::vector<float>& j, float x, bool jackstam);
-	JackSeq SequenceJack(const std::vector<NoteInfo>& NoteInfo, int t);
+	void JackStamAdjust(float x, int t, int mode, bool debug = false);
+	float JackLoss(float x, int mode, float mpl, bool stam, bool debug = false);
+	void SequenceJack(const Finger& f, int track, int mode);
 
-	int numitv;
-	int fastwalk(const std::vector<NoteInfo>& NoteInfo);
+	bool debugmode = false;
+	bool ssr = true; // set to true for scores, false for cache
+	int numitv = 0;
 
 	/*	Splits up the chart by each hand and calls ProcessFinger on each "track"
-	before passing
-	the results to the hand initialization functions. Also passes the input
-	timingscale value. */
-	void InitializeHands(const std::vector<NoteInfo>& NoteInfo, float ts);
+	before passing the results to the hand initialization functions. Also passes
+	the input timingscale value. Hardcode a limit for nps and if we hit it just
+	return max value for the calc and move on, there's no point in calculating
+	values for 500 nps joke files. Mem optimization can be better if we only
+	allow 100 maximum notes for a single half second interval, return value is
+	whether or not we should continue calculation */
+	bool InitializeHands(const std::vector<NoteInfo>& NoteInfo,
+						 float music_rate,
+						 float offset);
 
 	/*	Slices the track into predefined intervals of time. All taps within each
 	interval have their ms values from the last note in the same column
 	calculated and the result is spit out
-	into a new Finger object, or std::vector of std::vectors of floats (ms from last note
+	into a new Finger object, or vector of vectors of floats (ms from last note
 	in the track). */
-	Finger ProcessFinger(const std::vector<NoteInfo>& NoteInfo, int t);
-
-	// How many buttons do you press for this chart (currently hardcoded,
-	// clearly)
-	int numTracks = 4;
-
-	float vb = 0.f;
-	float vt = 0.f;
+	Finger ProcessFinger(const std::vector<NoteInfo>& NoteInfo,
+						 unsigned int t,
+						 float music_rate,
+						 float offset,
+						 bool& joke_file_mon);
 
 	// Derivative calc params
-	float MusicRate = 1.f;
-	float MaxPoints = 0.f; // Total points achievable in the file
-	float Scoregoal =
-	  0.93f; // What proportion of the total points are we trying to get
-	const float toffset = 0.f;
+	int MaxPoints = 0;	 // Total points achievable in the file
 	void TotalMaxPoints(); // Counts up the total points and assigns it
 
-	/*	Recursive non-linear calculation function. A player skill is asserted
-	and the calcultor calls the calcinternal functions for each hand and adds up
-	the calculated average points attained per attempt at the given skill level
-	for the given chart. This function will iterate until the percentage
-	obtained is greater than or equal to the scoregoal variable. The output
-	accuracy resolution can be set by either reducing the initial increment or
-	by increasing the starting iteration. */
-	float Chisel(float pskill,
-				 float res,
-				 int iter,
-				 bool stam,
-				 bool jack,
-				 bool nps,
-				 bool js,
-				 bool hs);
+	/*	Returns estimate of player skill needed to achieve score goal on chart.
+	 *  The player_skill parameter gives an initial guess and floor for player
+	 * skill. Resolution relates to how precise the answer is. Additional
+	 * parameters give specific skill sets being tested for.*/
+	float Chisel(float player_skill,
+				 float resolution,
+				 float score_goal,
+				 int ss, // skillset
+				 bool stamina,
+				 bool debugoutput = false);
 
-	std::vector<float> OHJumpDownscaler(const std::vector<NoteInfo>& NoteInfo,
-								   int t1,
-								   int t2);
-	std::vector<float> Anchorscaler(const std::vector<NoteInfo>& NoteInfo,
-							   int t1,
-							   int t2);
-	std::vector<float> HSDownscaler(const std::vector<NoteInfo>& NoteInfo);
-	std::vector<float> JumpDownscaler(const std::vector<NoteInfo>& NoteInfo);
-	std::vector<float> RollDownscaler(Finger f1, Finger f2);
-	void Purge();
-	float techscaler = 0.97f;
+	void TheThingLookerFinderThing(const std::vector<NoteInfo>& NoteInfo,
+								   float music_rate,
+								   std::vector<float> doot[]);
 
+	JumpHandChordData gen_jump_hand_chord_data(
+	  const std::vector<NoteInfo>& NoteInfo);
+
+	// nerf psuedo chords that are flams into oblivion
+	void SetFlamJamMod(const std::vector<NoteInfo>& NoteInfo,
+					   std::vector<float> doot[],
+					   float& music_rate);
+
+	void SetStreamMod(const std::vector<NoteInfo>& NoteInfo,
+					  std::vector<float> doot[ModCount],
+					  float music_rate);
+
+	void SetAnchorMod(const std::vector<NoteInfo>& NoteInfo,
+					  unsigned int t1,
+					  unsigned int t2,
+					  std::vector<float> doot[ModCount]);
+
+	// no longer going to necessarily be downscalers - that they were
+	// was a structural flaw of the old calc
+	void SetHSMod(const JumpHandChordData& data,
+				  std::vector<float> doot[ModCount]);
+	void SetJumpMod(const JumpHandChordData& data,
+					std::vector<float> doot[ModCount]);
+	void SetCJMod(const JumpHandChordData& data,
+				  std::vector<float> doot[ModCount]);
+
+	// run pattern mods that require specific sequencing at the same time to
+	// avoid iterating through all rows of the noteinfo more than once
+	// ok well we do it once per hand and we can probably solve that but...
+	void SetSequentialDownscalers(const std::vector<NoteInfo>& NoteInfo,
+								  unsigned int t1,
+								  unsigned int t2,
+								  float music_rate,
+								  std::vector<float> doot[ModCount]);
+
+	void WideRangeRollScaler(const std::vector<NoteInfo>& NoteInfo,
+							 unsigned int t1,
+							 unsigned int t2,
+							 float music_rate,
+							 std::vector<float> doot[ModCount]);
+	void WideRangeJumptrillScaler(const std::vector<NoteInfo>& NoteInfo,
+								  unsigned int t1,
+								  unsigned int t2,
+								  float music_rate,
+								  std::vector<float> doot[ModCount]);
+	void WideRangeAnchorScaler(const std::vector<NoteInfo>& NoteInfo,
+							   float music_rate,
+							   std::vector<float> doot[ModCount]);
+	void WideRangeBalanceScaler(const std::vector<NoteInfo>& NoteInfo,
+								float music_rate,
+								std::vector<float> doot[ModCount]);
+	std::vector<std::vector<float>> jacks[4][4];
+	std::vector<std::vector<float>> stam_adj_jacks[4];
 	Hand left_hand;
 	Hand right_hand;
+
   private:
 	std::vector<std::vector<int>> nervIntervals;
 
@@ -179,27 +228,18 @@ class Calc
 	  true; // Do we moving average the pattern modifier intervals?
 	const float IntervalSpan = 0.5f; // Intervals of time we slice the chart at
 	const bool logpatterns = false;
-
-	float dumbvalue = 1.f;
-	int dumbcounter = 0;
-
-	JackSeq j0;
-	JackSeq j1;
-	JackSeq j2;
-	JackSeq j3;
+	float fingerbias = 1.f;
 };
 
 MINACALC_API std::vector<float>
-MinaSDCalc(const std::vector<NoteInfo>& NoteInfo,
-		   float musicrate,
-		   float goal);
+MinaSDCalc(const std::vector<NoteInfo>& NoteInfo, float musicrate, float goal);
 MINACALC_API MinaSD
 MinaSDCalc(const std::vector<NoteInfo>& NoteInfo);
+MINACALC_API void
+MinaSDCalcDebug(
+  const std::vector<NoteInfo>& NoteInfo,
+  float musicrate,
+  float goal,
+  std::vector<std::vector<std::vector<std::vector<float>>>>& handInfo);
 MINACALC_API int
 GetCalcVersion();
-
-/*
-To get debug output:
-1. Turn off the built in showlog window
-2. Do StepMania.exe >> out.txt in cmd
-*/
--- Models/NoteData/NoteDataStructures.h
+++ Models/NoteData/NoteDataStructures.h
@@ -9,6 +9,24 @@ struct NoteInfo
 	float rowTime;
 };
 
+struct NoteInfo2
+{
+	int notes;
+	int rowTime;
+};
+
+struct DifficultyRating
+{
+	float overall;
+	float stream;
+	float jumpstream;
+	float handstream;
+	float stamina;
+	float jack;
+	float chordjack;
+	float technical;
+};
+
 enum Skillset
 {
 	Skill_Overall,
@@ -26,78 +44,47 @@ enum Skillset
 // we do actually want to register these with lua i guess
 enum CalcPatternMod
 {
-	Stream,
-	JS, // pattern mod (values between 0-1)
-	// JSS, // pattern mod (values between 0-1)
-	// JSJ, // pattern mod (values between 0-1)
-	HS, // pattern mod (values between 0-1)
-	// HSS, // pattern mod (values between 0-1)
-	// HSJ, // pattern mod (values between 0-1)
-	CJ, // pattern mod (values between 0-1)
-	// CJS, // pattern mod (values between 0-1)
-	// CJJ, // pattern mod (values between 0-1)
-	CJDensity,
-	HSDensity,
-	CJOHAnchor,
-	OHJumpMod, // pattern mod (values between 0-1)
-	// OHJBaseProp,
-	// OHJPropComp,
-	// OHJSeqComp,
-	// OHJMaxSeq,
-	// OHJCCTaps,
-	// OHJHTaps,
-	CJOHJump,
-	// CJOHJPropComp,
-	// CJOHJSeqComp,
-	Balance, // pattern mod (values between 0.9 - ~ 1.1)
-	Roll,	 // pattern mod (values between 0-1)
-	RollJS,
+	OHJump, // pattern mod (values between 0-1)
+	Anchor, // pattern mod (values between 0.9 - ~ 1.1)
+	Roll,   // pattern mod (values between 0-1)
+	HS,		// pattern mod (values between 0-1)
+	HSS,		// pattern mod (values between 0-1)
+	HSJ,		// pattern mod (values between 0-1)
+	JS,   // pattern mod (values between 0-1)
+	JSS,		// pattern mod (values between 0-1)
+	JSJ,		// pattern mod (values between 0-1)
+	CJ,		// pattern mod (values between 0-1)
+	CJS,		// pattern mod (values between 0-1)
+	CJJ,		// pattern mod (values between 0-1)
+	StreamMod,
 	OHTrill,
-	VOHTrill,
 	Chaos,
 	FlamJam,
 	WideRangeRoll,
 	WideRangeJumptrill,
-	WideRangeJJ, // wrjumpjack
 	WideRangeBalance,
 	WideRangeAnchor,
+	CJOHJump,
+	CJQuad,
 	TheThing,
-	TheThing2,
-	RanMan,
-	Minijack,
-	// RanLen,
-	// RanAnchLen,
-	// RanAnchLenMod,
-	// RanJack,
-	// RanOHT,
-	// RanOffS,
-	// RanPropAll,
-	// RanPropOff,
-	// RanPropOHT,
-	// RanPropOffS,
-	// RanPropJack,
-	TotalPatternMod,
 	NUM_CalcPatternMod,
 	CalcPatternMod_Invalid,
 };
 enum CalcDiffValue
 {
-	NPSBase,
-	MSBase,
-	JackBase,
-	CJBase,
-	TechBase,
-	RMABase,
-	MSD,
+	BaseNPS, // unadjusted base nps difficulty
+	BaseMS,  // unadjusted base ms difficulty
+	BaseMSD, // unadjusted weighted values
+	MSD,	 // pattern and stam adjusted difficulty values
 	NUM_CalcDiffValue,
 	CalcDiffValue_Invalid,
 };
 enum CalcDebugMisc
 {
-	Pts, // points per interval is constant, but slightly multiplied for each skillset
 	PtLoss, // expected points loss (not really a diff thing but w.e)
-	StamMod, // stam adjust (values between 1- ~1.15)
-	// JackStamMod,
+	JackPtLoss,
+	StamMod,// stam adjust (values between 1- ~1.15)
+	JackStamMod,
 	NUM_CalcDebugMisc,
 	CalcDebugMisc_Invalid,
 };
